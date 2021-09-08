4d7c52e22e1dc8183aa738eec93b43855a85f310
tdrobinson94 <tdrobinson94@gmail.com>
2021-09-08 08:56:14 -0400

    add some observables and console logging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-rxjs';
+
+  ngOnInit(): void {
+    let observable = Observable.create((observer:any) => {
+      observer.next('Hello World!')
+      observer.next('I am number 1')
+      observer.next('I am number 2')
+      setInterval(() => {
+        observer.next('Random Async log message')
+      }, 2000)
+      // observer.error('I am number 3')
+      // observer.complete('I am number 4')
+      observer.next('I am number 5')
+    });
+
+    observable.subscribe((message:any) => {
+      console.log(message);
+    });
+  }
 }
